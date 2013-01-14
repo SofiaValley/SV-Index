719ecdac3f3af4ae53a94970119d95678e61629d
Kiril Markov <krivil@gmail.com>
2013-01-14 23:18:49 +0200

    BTree keyword match

---
+++ SV-PLI/SimpleTokenizer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SV_PLI
+{
+    public class SimpleTokenizer : ITokenizer
+    {
+        public IEnumerable<int> Tokenize(string input)
+        {
+            int i = 0;
+            if (String.IsNullOrEmpty(input))
+                yield break;
+
+            bool lastNonLetterOrDigit = true;
+
+            foreach (char c in input)
+            {
+                if (!Char.IsLetterOrDigit(c))
+                    lastNonLetterOrDigit = true;
+                else if (lastNonLetterOrDigit)
+                {
+                    lastNonLetterOrDigit = false;
+                    yield return i;
+                }
+                i++;
+            }
+        }
+    }
+}
